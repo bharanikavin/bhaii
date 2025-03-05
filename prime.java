a1c1876b8ed2a25a6d48cd3b2fb2d78e4d3e2f6a
Bharanikavin <bharanikavinpk@gmail.com>
2025-03-05 12:23:03 +0530

    java

---
+++ prime.java
@@ -0,0 +1,23 @@
+public class prime{
+    public static void main(String[] args) {
+        int num = 17;
+        int count =0;
+       if(num<=1){
+        System.out.println("not a prime");
+       }
+       else{
+       for(int i=2;i<=num;i++){
+        if(num%i==0){
+            count++;
+        }
+
+       if(count>1){
+        System.out.println("not a prime");
+       }
+       else{
+        System.out.println("prime");
+       }
+    }
+    }
+}
+}
